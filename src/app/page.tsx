1cf34ca12dd6edc123c37c9e6484315e2bbe52ff
Weslley Sniper <weslley.kacau@gmail.com>
2025-07-26 02:00:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -140,7 +140,7 @@ export default function Home() {
                     <DynamicTabsList className="h-auto md:h-12 rounded-xl p-1 flex-wrap md:flex-nowrap">
                         <DynamicTabsTrigger value="calculator" className="text-base font-semibold rounded-lg flex items-center gap-2 px-4 py-2 md:py-1.5">
                             <Dog />
-                            Calculadora
+                            Calcular
                         </DynamicTabsTrigger>
                         <DynamicTabsTrigger value="chatbot" className="text-base font-semibold rounded-lg flex items-center gap-2 px-4 py-2 md:py-1.5">
                             <MessageSquare />
